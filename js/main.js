f01d26dbdef3ee376691a0e05c9a692257c96e09
maplemegan333 <maplemegan@ufl.edu>
2018-04-08 16:08:15 -0400

    Style paragraphs

---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    var paragraphs = $('p');
+
+    $(paragraphs).css({'background-color': '#003249', 'color': '#CCDBDC',
+        'width': '75%', 'margin': 'auto'});
+});
